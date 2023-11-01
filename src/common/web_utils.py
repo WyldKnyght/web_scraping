4f7c16d6c6c29c32bc19964f762c7131d4e7df62
Sourcery AI <>
2023-11-01 16:12:54 +0000

    'Refactored by Sourcery'

--- src/common/web_utils.py
+++ src/common/web_utils.py
@@ -4,21 +4,13 @@ import requests
 from bs4 import BeautifulSoup
 
 def send_get_request(url):
-    # Send a GET request to the URL
-    response = requests.get(url)
-    return response
+    return requests.get(url)
 
 def parse_html(response):
-    # Create a BeautifulSoup object to parse the HTML
-    soup = BeautifulSoup(response.text, "html.parser")
-    return soup
+    return BeautifulSoup(response.text, "html.parser")
 
 def extract_text(soup):
-    # Extract all the text from the website
-    text = soup.get_text()
-    return text
+    return soup.get_text()
 
 def get_website_name(url):
-    # Get the website name from the URL
-    website_name = url.split("//")[-1].split("/")[0]
-    return website_name
+    return url.split("//")[-1].split("/")[0]
